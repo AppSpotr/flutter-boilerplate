9b4e1345f16539d6e1d85874a17ea0b8bb38f9a7
kevin <kevin@twire.se>
2022-07-15 03:17:23 -0500

    [added] dotenv and empty widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:boilerplate/screens/home/widget.dart';
-import 'package:boilerplate/state/navigation/actions.dart';
 import 'package:boilerplate/state/navigation/state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+Future main() async {
+  await dotenv.load(fileName: '.env');
+
   runApp(const MyApp());
 }
 
